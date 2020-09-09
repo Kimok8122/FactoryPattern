260cac268a2f6c4a8ccf9cabfb9e3cc2710686cf
Mona Lisa <email@example.com>
2020-09-09 14:03:37 -0600

    starting

---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int wheelCount)
+        {
+            switch (wheelCount)
+            {
+                case 4:
+                    return new Car();
+
+                case 2:
+                    return new Motorcycle();
+
+                case 3:
+                    return new ThreeWheeler();
+
+                default:
+                    return new Car();
+
+
+ 
+            }
+        }
+
+
+            
+    }
+}
